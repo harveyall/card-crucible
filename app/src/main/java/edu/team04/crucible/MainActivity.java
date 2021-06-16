798bb15e448904c22d36b25a120782df6d9aafa1
harveyall <allharvey@byui.edu>
2021-06-15 19:54:54 -0600

    Initial commit, main activity with settings button.

---
+++ app/src/main/java/edu/team04/crucible/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.team04.crucible;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
