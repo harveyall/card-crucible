e30c3a614ea61de5e2953b3fbabb6bc4ddb26468
alouisa <annaastle5@gmail.com>
2021-06-16 15:04:02 -0700

    Added not yet added empty activities and classes from our UML

---
+++ app/src/main/java/edu/team04/crucible/StudyModeActivity.java
@@ -0,0 +1,14 @@
+package edu.team04.crucible;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudyModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study);
+    }
+}
