3ab575ab9980978a9bb71c590768a0c1ce65b075
alouisa <annaastle5@gmail.com>
2021-06-16 18:16:12 -0700

    Added member variables and functions to Category and Card classes

--- app/src/main/java/edu/team04/crucible/Card.java
+++ app/src/main/java/edu/team04/crucible/Card.java
@@ -1,4 +1,37 @@
 package edu.team04.crucible;
 
 public class Card {
+    private int id;
+    private String question;
+    private String answer;
+
+    public Card(int id, String question, String answer){
+        this.id = id;
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
 }
--- app/src/main/java/edu/team04/crucible/Category.java
+++ app/src/main/java/edu/team04/crucible/Category.java
@@ -1,4 +1,51 @@
 package edu.team04.crucible;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Category {
+
+    private int id;
+    private String name;
+    private List<Card> cards;
+
+    public Category(int id, String name){
+        this.id = id;
+        this.name = name;
+        this.cards = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public Card getCard(int index){
+        //returns a specific card by index
+        return this.cards.get(index);
+    }
+
+    public void addCard(Card card){
+        this.cards.add(card);
+    }
+
+    public void removeCard(Card card){
+        this.cards.remove(card);
+    }
+
 }
