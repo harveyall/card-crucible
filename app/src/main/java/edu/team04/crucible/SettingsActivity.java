dfa12d02f519a28908b770d7d63353011e5d7438
alouisa <annaastle5@gmail.com>
2021-06-19 09:31:31 -0700

    Added TODOS in activities. If they aren't helpful feel free to delete them or edit them in a way that is more helpful to you.

--- app/src/main/java/edu/team04/crucible/SettingsActivity.java
+++ app/src/main/java/edu/team04/crucible/SettingsActivity.java
@@ -8,6 +8,12 @@ import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    //TODO: When Begin button is pressed start EditCategoryActivity
+    // and send it the selected Category's Card List
+
+    //TODO: If add Category button is pressed add new Category
+    // if this Activity needs to make any requests for information, do in handler/presenter class
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
