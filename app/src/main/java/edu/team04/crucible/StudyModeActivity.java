c953821b233408561bd896220a58cd9ad268f2aa
Sean Kirkland <conconcraft@byui.edu>
2021-06-24 18:35:41 -0600

    Minor improvments.

--- app/src/main/java/edu/team04/crucible/StudyModeActivity.java
+++ app/src/main/java/edu/team04/crucible/StudyModeActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -13,10 +12,10 @@ import android.widget.TextView;
 public class StudyModeActivity extends AppCompatActivity {
     //TODO: Begin game with card list from selected category that is sent from SelectCategory Activity
     // use StudyModeHandler to handle requests from this activity
-    private TextView frontCard;
-    private TextView backCard;
+    final TextView frontCard = (TextView) findViewById(R.id.card_front);
+    final TextView backCard = (TextView) findViewById(R.id.card_back);
 
-    private Button aButton;
+    final Button aButton = findViewById(R.id.flip_btn);
 
     private AnimatorSet frontAnim;
     private AnimatorSet backAnim;
@@ -35,24 +34,21 @@ public class StudyModeActivity extends AppCompatActivity {
         frontAnim = (AnimatorSet) AnimatorInflater.loadAnimator(getApplicationContext(),R.animator.front_animatior);
         backAnim = (AnimatorSet) AnimatorInflater.loadAnimator(getApplicationContext(),R.animator.back_animatior);
 
-        aButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        aButton.setOnClickListener(view -> {
 
-                if (isFront) {
-                    frontAnim.setTarget(frontCard);
-                    backAnim.setTarget(backCard);
-                    frontAnim.start();
-                    backAnim.start();
-                    isFront = false;
-                }
-                else {
-                    frontAnim.setTarget(backCard);
-                    backAnim.setTarget(frontCard);
-                    backAnim.start();
-                    frontAnim.start();
-                    isFront = true;
-                }
+            if (isFront) {
+                frontAnim.setTarget(frontCard);
+                backAnim.setTarget(backCard);
+                frontAnim.start();
+                backAnim.start();
+                isFront = false;
+            }
+            else {
+                frontAnim.setTarget(backCard);
+                backAnim.setTarget(frontCard);
+                backAnim.start();
+                frontAnim.start();
+                isFront = true;
             }
         });
     }
