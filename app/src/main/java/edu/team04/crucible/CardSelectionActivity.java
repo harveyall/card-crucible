af7504a2992f300aec93de93ffbefb54cbb23d8d
harveyall <allharvey@byui.edu>
2021-06-24 19:13:58 -0600

    Adding Javadoc to Begin button on CategorySelectionActivity.

--- app/src/main/java/edu/team04/crucible/CardSelectionActivity.java
+++ app/src/main/java/edu/team04/crucible/CardSelectionActivity.java
@@ -13,17 +13,22 @@ import androidx.appcompat.app.AppCompatActivity;
 public class CardSelectionActivity extends AppCompatActivity {
     //TODO: When begin button is clicked send selected category to StudyModeActivity
 
-    public void Begin(View view) {
-    Intent intent = new Intent(CardSelectionActivity.this, StudyModeActivity.class);
-    //TODO: Populate Listview/Recyclerview with Card Categories.
-    //TODO: Add selected Categories to intent.
-        startActivity(intent);
-    }
         @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ((android.widget.Button)findViewById(R.id.button_begin)).setText(R.string.study_mode);
         setContentView(R.layout.activity_card_selection);
+    }
+
+    /**
+     * This button takes the selected categories into the next Activity (Study or Game).
+     * @param view
+     */
+    public void Begin(View view) {
+        Intent intent = new Intent(CardSelectionActivity.this, StudyModeActivity.class);
+        //TODO: Populate Listview/Recyclerview with Card Categories.
+        //TODO: Add selected Categories to intent.
+        startActivity(intent);
     }
 
     /**
