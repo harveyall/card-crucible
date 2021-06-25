c081fc44174196c3d8fe031ec6dfe5c5dc8121b2
harveyall <allharvey@byui.edu>
2021-06-24 18:36:57 -0600

    Adding basic navigation from MainActivity to the related. Adding Javadocs to MainActivity.

--- app/src/main/java/edu/team04/crucible/MainActivity.java
+++ app/src/main/java/edu/team04/crucible/MainActivity.java
@@ -1,18 +1,51 @@
 package edu.team04.crucible;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+/**
+ * The Main Activity, with buttons for Settings, Study Mode & Game Mode.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ((android.widget.Button)findViewById(R.id.button_begin)).setText(R.string.study_mode);
+        ((android.widget.Button)findViewById(R.id.button_begin)).setText(R.string.game_mode);
         setContentView(R.layout.activity_main);
     }
 
-    //TODO: add onClick function for Settings button
+    /**
+     * Button to Settings Activity.
+     * @param button
+     */
+    public void settings (View button) {
+        Intent settingsActivity = new Intent(this, SettingsActivity.class);
+        startActivity(settingsActivity);
+    }
 
-    //TODO: add onClick function for Category Selection button
+    /**
+     * Button to Card Selection Activity with Study Mode intent.
+     * @param button
+     */
+    public void cardSelectForStudy (View button) {
+        //TODO: Pass the Study mode intent.
+        Intent cardSelectionActivity = new Intent(this, CardSelectionActivity.class);
+        startActivity(cardSelectionActivity);
+    }
+
+    /**
+     * Button to Card Selection Activity with Game Mode intent.
+     * @param button
+     */
+    public void cardSelectForGame (View button) {
+        //TODO: Pass the Game mode intent.
+        Intent cardSelectionActivity = new Intent(this, CardSelectionActivity.class);
+        startActivity(cardSelectionActivity);
+    }
+
 }
