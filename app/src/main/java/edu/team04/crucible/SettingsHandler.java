226d7d7db7bb6576c4d4b3c87324f59f96606ec8
E-Laredo <enriquelaredo@byui.edu>
2021-06-29 18:32:34 -0600

    Cleaning up unneeded methods on SettingsHandler. Checking Javadocs on SettingsActivity Cleaning up of formatting on settings_activity.xml

--- app/src/main/java/edu/team04/crucible/SettingsHandler.java
+++ app/src/main/java/edu/team04/crucible/SettingsHandler.java
@@ -4,41 +4,25 @@ import android.content.Context;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This class takes care of any requests made by the SettingsActivity
  */
-public class SettingsHandler {
+public class SettingsHandler implements Runnable {
+    private SettingsActivity activity;
+    private Context context;
 
-    /*public static void writeToFile(Context context, String fileName, String str) {
-        try {
-            fileOutputStream outstream = context.openFileOutput(fileName, context.MODE_PRIVATE);
-            outstream.write(str.getBytes(), 0, str.length());
-            outstream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void serializeData(View view) {
-        Gson gson = new Gson();
-        String JsonString = gson.toJson(categ);
-        IOHelper.writeToFile(this, "CategoryData.txt", JsonString;
-    }
-
-    public void deSerializeData (View view) {
-        Gson gson = new Gson();
-        try {
-            FileInputStream stream = openFileInput("CategoryData.txt");
-            String data = IOHelper.stringFromStream(stream);
-            Category fileCat = gson.fromJson(data, Category.class);
-            Log.d("SettingsActivity", "id: " + fileCat.getId() + "Name: " + fileCat.getName() + "\n"
-                    + "Cards: " + fileCat.getCards();
-            stream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } return fileCat;
-
-    }
+    /** Class constructor, it would take the calling activity's name, to then return the
+     * results on that activity's UI thread.
+     * @param activity The calling activity's name.
      */
+    public SettingsHandler(SettingsActivity activity) {
+        this.activity = activity;
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
