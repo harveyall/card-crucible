a986c1924ef2cb0b2b9e49aac7ce32aeae7da649
Sean Kirkland <conconcraft@byui.edu>
2021-06-30 19:22:35 -0600

    Some simplification. Added Visible Varible to be used on Prev and Next Button.

--- app/src/main/java/edu/team04/crucible/StudyModeActivity.java
+++ app/src/main/java/edu/team04/crucible/StudyModeActivity.java
@@ -3,6 +3,7 @@ package edu.team04.crucible;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,7 +15,9 @@ import androidx.appcompat.app.AppCompatActivity;
 public class StudyModeActivity extends AppCompatActivity {
     //TODO: Begin game with card list from selected category that is sent from SelectCategory Activity
     // use StudyModeHandler to handle requests from this activity
-    private boolean visible = false;
+    private boolean back_card_visible = false;
+    private boolean prev_button_visible = true;
+    private boolean next_button_visible = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +26,19 @@ public class StudyModeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_study);
         float scale = getApplicationContext().getResources().getDisplayMetrics().density;
 
-        TextView backCard = (TextView)findViewById(R.id.card_back);
-        backCard.setText("Back");
-        backCard.setVisibility(View.INVISIBLE);  // For Invisible/Disappear
         TextView frontCard = (TextView)findViewById(R.id.card_front);
+        TextView backCard = (TextView)findViewById(R.id.card_back);
+
+        backCard.setVisibility(View.INVISIBLE);  // For Invisible/Disappear
+
         frontCard.setText("Front");
+        backCard.setText("Back");
+
+        View btn_prev = findViewById(R.id.prev_btn);
+        btn_prev.setVisibility(View.VISIBLE);
+
+        View btn_next = findViewById(R.id.next_btn);
+        btn_next.setVisibility(View.VISIBLE);
     }
 
     /**
@@ -36,25 +47,13 @@ public class StudyModeActivity extends AppCompatActivity {
      */
     public void flip(View button) {
         TextView backCard = (TextView)findViewById(R.id.card_back);
-        if(visible) {
+        if(back_card_visible) {
             backCard.setVisibility(View.INVISIBLE);  // For Invisible/Disappear
-            visible = false;
+            back_card_visible = false;
         }
         else {
             backCard.setVisibility(View.VISIBLE); // For Visible/Appear
-            visible = true;
-        }
-    }
-
-    /**
-     * This button moves to the next side and will set the card to being face up.
-     * @param button
-     */
-    public void next(View button) {
-        TextView backCard = (TextView)findViewById(R.id.card_back);
-        if(visible) {
-            backCard.setVisibility(View.INVISIBLE);  // For Invisible/Disappear
-            visible = false;
+            back_card_visible = true;
         }
     }
 
@@ -64,9 +63,33 @@ public class StudyModeActivity extends AppCompatActivity {
      */
     public void prev(View button) {
         TextView backCard = (TextView)findViewById(R.id.card_back);
-        if(visible) {
+        View btn_prev = findViewById(R.id.prev_btn);
+
+        if(!prev_button_visible) { //Remove the ! in latter version
+            btn_prev.setVisibility(View.GONE);
+            prev_button_visible = false;
+        }
+        if(back_card_visible) {
             backCard.setVisibility(View.INVISIBLE);  // For Invisible/Disappear
-            visible = false;
+            back_card_visible = false;
+        }
+    }
+
+    /**
+     * This button moves to the next side and will set the card to being face up.
+     * @param button
+     */
+    public void next(View button) {
+        TextView backCard = (TextView)findViewById(R.id.card_back);
+        View btn_next = findViewById(R.id.next_btn);
+
+        if(!next_button_visible) { //Remove the ! in latter version
+            btn_next.setVisibility(View.GONE);
+            next_button_visible = false;
+        }
+        if(back_card_visible) {
+            backCard.setVisibility(View.INVISIBLE);  // For Invisible/Disappear
+            back_card_visible = false;
         }
     }
 
