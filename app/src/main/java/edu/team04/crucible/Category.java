feb1282765e5cc32fc920ffb3223608aa8c71761
harveyall <allharvey@byui.edu>
2021-07-08 19:37:50 -0600

    Adding Javadocs to the Category Class.

--- app/src/main/java/edu/team04/crucible/Category.java
+++ app/src/main/java/edu/team04/crucible/Category.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * A Category of flashcards, with a name and a list of cards associated with it.
+ */
 public class Category extends Selectable {
 
     private String name;
@@ -11,49 +14,55 @@ public class Category extends Selectable {
 
     /**
      * Constructs a Category with a specified name
-     * @param name
+     * @param name The provided name string.
      */
     public Category(String name){
         this.name = name;
         this.cards = new ArrayList<>();
     }
 
-    /** Return the name of this category */
+    /**
+     * This method returns the name of the category.
+     * @return The category's name string.
+     */
     public String getName() {
         return name;
     }
 
-    /** Set the name of this category to the specified string value
-     * @param name
+    /**
+     * Sets the name of this category to the specified string value
+     * @param name The provided name string.
      */
     public void setName(String name) {
         this.name = name;
     }
 
-    /** Return this category's list of cards */
+    /**
+     * Returns this category's list of cards
+     * @return A list of the Card objects.
+     */
     public List<Card> getCards() {
         return cards;
     }
 
     /** Using a card's index, return that card from this category's list of cards
-     * @param index
+     * @param index The provided index.
      */
     public Card getCard(int index){
         //returns a specific card by index
         return this.cards.get(index);
     }
 
-    /** Replace an card with it's edited version using the index of the original card
-     * @param oldCardIndex
-     * @param editedCard
+    /** This method replaces a card with it's edited version using the index of the original card.
+     * @param oldCardIndex The provided index for the old card.
+     * @param editedCard The edited Card object.
      */
     public void replaceCard(int oldCardIndex, Card editedCard){
-        //replaces old card with edited version
         this.cards.set(oldCardIndex, editedCard);
     }
 
     /** Add a card for this category's list of cards
-     * @param card
+     * @param card A Card object.
      * */
     public void addCard(Card card){
         if(this.canAddCards()) {
@@ -62,28 +71,26 @@ public class Category extends Selectable {
     }
 
     /**
-     * Shuffles the cards belonging to the specified Category.
-     * @param toRandomize The Category which cards are to be shuffled.
-     * @return The same Category after the cards have been shuffled.
+     * This method returns true if there are less
+     * than 50 cards in this category's list of cards and false if not
+     * @return True or False.
      */
-    public Category randomizeCategory(Category toRandomize) {
-        Collections.shuffle(toRandomize.getCards());
-        return toRandomize;
-    }
-
-    /** This method returns true if there are less than 50 cards in this category's list of cards and false if not */
     public boolean canAddCards(){
         return this.cards.size() < 50;
     }
 
-    /** Remove a card for this category's list of cards
-     * @param card
+    /** This method removes a card for this category's list of cards
+     * @param card A Card object.
      * */
     public void removeCard(Card card){
         this.cards.remove(card);
     }
 
-
+    /**
+     * This method returns true if the specified card is found within this category.
+     * @param card A Card object.
+     * @return True or False.
+     */
     public boolean containsCard(Card card){
         for(Card cardItem : this.cards) {
             if(cardItem.isEqual(card)){
@@ -93,7 +100,19 @@ public class Category extends Selectable {
         return false;
     }
 
-    /** Return a string representing this category*/
+    /**
+     * This method shuffles the provided List of Cards.
+     * @param toRandomize The List which cards are to be shuffled.
+     * @return The List after the cards have been shuffled.
+     */
+    public Category randomizeCards(Category toRandomize) {
+        Collections.shuffle(toRandomize.getCards());
+        return toRandomize;
+    }
+
+    /**
+     * Returns a string representing this category
+     */
     @Override
     public java.lang.String toString() {
         return "Category{" +
