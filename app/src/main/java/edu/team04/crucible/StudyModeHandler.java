4d9b6b8f760d38013c2da268c3cea56ffac2c348
alouisa <annaastle5@gmail.com>
2021-07-13 09:08:56 -0700

    StudyModeHandler.java: converted categoryList from StudyModeActivity.java into a cardList.

--- app/src/main/java/edu/team04/crucible/StudyModeHandler.java
+++ app/src/main/java/edu/team04/crucible/StudyModeHandler.java
@@ -6,11 +6,19 @@ import android.content.Intent;
 
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudyModeHandler<categoryList> implements Runnable{
     Activity activity;
+    Context context;
     CategoryList categoryList;
+    List<Card> cards;
     public StudyModeHandler(Activity activity, Context context, CategoryList categoryList){
+        this.activity = activity;
+        this.context = context;
         this.categoryList = categoryList;
+        this.cards = createCardList();
     }
 
     @Override
@@ -18,4 +26,20 @@ public class StudyModeHandler<categoryList> implements Runnable{
 
     }
     //This class takes care of any requests by the StudyModeHelper
+
+
+    //TODO: remove this method later if it gets used instead in the CardSelectionActivity.
+    /** Takes the categoryList returned by the cardSelectionActivity and turns it into a cardList*/
+    public ArrayList<Card> createCardList(){
+        ArrayList<Card> cards = new ArrayList<>();
+        if(categoryList != null) {
+            for (Category category : this.categoryList.getCategories()) {
+                cards.addAll(category.getCards());
+            }
+        }
+        else{
+            cards = null;
+        }
+        return cards;
+    }
 }
