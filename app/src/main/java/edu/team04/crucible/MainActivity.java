b2688957edfecee36ac47fc14cfa999a6d28974b
E-Laredo <enriquelaredo@byui.edu>
2021-07-12 20:07:44 -0600

    MainActivity. Adding implementation of notification every time the user goes to the Main Activity. Notification set to happen shortly for testing purposes. Could not implement alarm setting outside of activity.

--- app/src/main/java/edu/team04/crucible/MainActivity.java
+++ app/src/main/java/edu/team04/crucible/MainActivity.java
@@ -1,15 +1,26 @@
 package edu.team04.crucible;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 /**
  * The Main Activity, with buttons for Settings, Study Mode & Game Mode.
  */
 public class MainActivity extends AppCompatActivity {
+    public static final String NOTIFICATION_CHANNEL_ID = "10001";
+    private final static String default_notification_channel_id = "default";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
         TestData test = new TestData(this);
         new Thread(test, "TestCategoryList").start();
+        scheduleNotification(getNotification("It's Study time!"), 20000);
     }
 
     /**
@@ -50,5 +62,29 @@ public class MainActivity extends AppCompatActivity {
         Intent cardSelectionActivity = new Intent(this, CardSelectionActivity.class);
         cardSelectionActivity.putExtra("ACTIVITY","Game");
         startActivity(cardSelectionActivity);
+    }
+
+    private void scheduleNotification(Notification notification, int delay) {
+        Intent notificationIntent = new Intent(this, NotificationPublisher.class);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION_ID, 1);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION, notification);
+        PendingIntent pendingIntent = PendingIntent. getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        long futureInMillis = SystemClock.elapsedRealtime() + delay;
+        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        assert alarmManager != null;
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
+    }
+    private Notification getNotification(String content) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, default_notification_channel_id);
+        builder.setContentTitle("Card Crucible");
+        builder.setContentText(content);
+        builder.setSmallIcon(R.drawable.card_crucible_app_icon);
+        builder.setVibrate(new long[]{500, 500});
+        builder.setLights(Color.BLUE, 750, 250);
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        builder.setSound(alarmSound);
+        builder.setAutoCancel(true);
+        builder.setChannelId(NOTIFICATION_CHANNEL_ID);
+        return builder.build();
     }
 }
