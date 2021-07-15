1cff7bc075742e97dcf7e4e4ce0900db1d8c166b
alouisa <annaastle5@gmail.com>
2021-07-15 11:12:13 -0700

    EditSelectionAdapter: Removed filterBtn onClick to EditSelectionAdapter (caused program crash)

--- app/src/main/java/edu/team04/crucible/EditSelectionAdapter.java
+++ app/src/main/java/edu/team04/crucible/EditSelectionAdapter.java
@@ -41,19 +41,15 @@ public class EditSelectionAdapter extends
         public CardView cv_editableItem;
         public Button editBtn;
         public Button deleteBtn;
-        public Button filterBtn;
         public TextView categoryText;
         public TextView questionText;
         public TextView answerText;
-        public EditText categoryFilter;
 
         public ViewHolder(@NonNull @NotNull View itemView) {
             super(itemView);
-            categoryFilter = (EditText)itemView.findViewById(R.id.editTextTextCategoryFilter);
             cv_editableItem = (CardView) itemView.findViewById(R.id.cv_editableItem);
             editBtn = (Button) itemView.findViewById(R.id.button_editCard);
             deleteBtn = (Button) itemView.findViewById(R.id.button_deleteCard);
-            filterBtn = (Button) itemView.findViewById(R.id.button_cardFilter);
             categoryText = (TextView) itemView.findViewById(R.id.textView_category);
             questionText = (TextView) itemView.findViewById(R.id.textView_question);
             answerText = (TextView) itemView.findViewById(R.id.textView_answer);
@@ -98,12 +94,6 @@ public class EditSelectionAdapter extends
                 Log.d("EditSelectionAdapter", "Beginning removeCardHandler thread");
                 Thread removeCardInHandler = new Thread(new EditSelectionHandler(context, categoryList, card));
                 removeCardInHandler.start();
-            }
-        });
-        holder.filterBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String categoryName = holder.categoryFilter.getText().toString();
             }
         });
     }
