f049f87d815aa60a37ff675ea3c7fd2c93d2da70
alouisa <annaastle5@gmail.com>
2021-07-19 08:49:45 -0700

    SharedPreferenceManager: additional javaDoc

--- app/src/main/java/edu/team04/crucible/SharedPreferenceManager.java
+++ app/src/main/java/edu/team04/crucible/SharedPreferenceManager.java
@@ -6,10 +6,15 @@ import android.content.SharedPreferences;
 import androidx.preference.PreferenceManager;
 
 public class SharedPreferenceManager {
+    /** Set the specified integer representation of the theme in shared preferences
+     * @param context
+     * @param theme
+     * */
     public static void setTheme(Context context, int theme){
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
         sp.edit().putInt("theme", theme).apply();
     }
+    /** Return the integer representation of the theme saved in shared preferences * */
     public static int getTheme(Context context) {
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
         return sp.getInt("theme", -1);
