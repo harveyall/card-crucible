b17a7c1ed6077dcadef3d49077ce2e554e35a0d2
alouisa <annaastle5@gmail.com>
2021-07-19 08:33:37 -0700

    EditSelectionHandler: Added additional javadoc

--- app/src/main/java/edu/team04/crucible/EditSelectionHandler.java
+++ app/src/main/java/edu/team04/crucible/EditSelectionHandler.java
@@ -11,11 +11,22 @@ public class EditSelectionHandler implements Runnable{
     Context context;
     CategoryList categoryList;
     Card cardToDelete;
+    /** Construct an EditSelectionHandler with a specified context,
+     * list of categories currently saved in local storage,
+     * and a card to be deleted from that category list storage
+     * @param context
+     * @param savedCategoryList
+     * @param cardToDelete*/
     public EditSelectionHandler(Context context, CategoryList savedCategoryList, Card cardToDelete){
         this.context = context;
         this.categoryList = savedCategoryList;
         this.cardToDelete = cardToDelete;
     }
+    /** When this class is created get the category in the categoryList that is associated with the card to be deleted
+     * Remove the card for that category
+     * Is the category is now empty, remove it from the categoryList
+     * Save the modifed categoryList
+     * */
     @Override
     public void run() {
         Category currentCategory = this.categoryList.getCategory(this.cardToDelete.getCategory());
